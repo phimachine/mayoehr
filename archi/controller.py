f1aba1c364a4b88ff3049d4426780b3daa5b3ced
Fuchai <huyaojie168@me.com>
2018-02-25 01:13:25 -0600

    Controller draft done

--- archi/controller.py
+++ archi/controller.py
@@ -13,24 +13,30 @@ class Controller(nn.Module):
     """
     def __init__(self):
         super(Controller, self).__init__()
+        self.RNN_list=nn.ModuleList()
+        for _ in param_L:
+            self.RNN_list.append(RNN_Unit())
+        self.hidden_previous_timestep=torch.Tensor(param_L*param_h).zero_()
+        self.W_y=nn.Linear(param_L*param_h,param_v_t)
+        self.W_E=nn.Linear(param_L*param_h,param_E_t)
 
+    def forward(self, input_x):
+        # the units cannot be calculated with a big matrix.
+        # since each unit will need previous unit's output, the calculation must
+        # be time-differentiated
+        # not sure whether this is the most efficient loop implementation.
+        hidden_previous_layer=torch.Tensor(param_h).zero_()
+        hidden_this_timestep=torch.Tensor(param_L*param_h)
+        for i in param_L:
+            hidden_output=self.RNN_list[i](input_x, self.all_previous_time[i],
+                             hidden_previous_layer)
+            # TODO verify this line, see if slicing is possible. torch.narrow()
+            hidden_this_timestep[i*param_h:(i+1)*param_h]=hidden_output
+            hidden_previous_layer=hidden_output
 
-    def forward(self, input):
-        """
-
-        :param input:
-        x_t: [X], input vector at time step t
-
-        
-
-        :return:output:
-        v_t
-
-        """
-
-
-        output=None
-        interface=None
+        output=self.W.y(hidden_this_timestep)
+        interface=self.W_E(hidden_this_timestep)
+        self.hidden_previous_timestep=hidden_this_timestep
         return output, interface
 
 
@@ -50,21 +56,27 @@ class RNN_Unit(nn.Module):
         self.W_state=nn.Linear(param_s,param_x+2*param_h)
         self.b_state=nn.Parameter(torch.tensor(param_s))
 
-        self.old_state=Parameter(torch.Tensor(param_s))
+        # state or cell, initialized in place to zero.
+        self.old_state=Parameter(torch.Tensor(param_s).zero_())
 
     def reset_parameters(self):
         pass
 
     def forward(self,input_x,previous_time,previous_layer):
+        # a hidden unit outputs a hidden output new_hidden.
+        # state also changes, but it's hidden inside a hidden unit.
+
         semicolon_input=torch.cat(input_x,previous_time,previous_layer)
+
+        # 5 equations
         input_gate=torch.nn.Sigmoid(self.W_input(semicolon_input)+self.b_input)
         forget_gate=torch.nn.Sigmoid(self.W_forget(semicolon_input)+self.b_forget)
-        output_gate=torch.nn.Sigmoid(self.W_output(semicolon_input)+self.b_output)
-
         new_state=forget_gate * self.old_state + input_gate * \
                    nn.Tanh(self.W_state(semicolon_input)+ self.b_state)
+        output_gate=torch.nn.Sigmoid(self.W_output(semicolon_input)+self.b_output)
         new_hidden=output_gate*nn.Tanh(new_state)
 
+        return new_hidden
 
 
 
--- archi/parameters.py
+++ archi/parameters.py
@@ -7,7 +7,7 @@ Controller parameters
 # input vector size x_t
 param_x=10
 
-# single hidden unit size h^l_t
+# single hidden unit output size h^l_t
 param_h=10
 
 # state size
@@ -15,6 +15,11 @@ param_h=10
 # all weight matrices in equation 1-5 then has dimension (param_s, param_x+2*param_h)
 param_s=20
 
-
-# RNN layers count
+# Controller RNN layers count
 param_L=64
+
+# Controller output v_t size
+param_v_t=32
+
+# Controller interface epsilon_t size
+param_E_t=32
