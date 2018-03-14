22533580e557110cf8d330be2818729b8f30d626
Jason Hu <huyaojie168@me.com>
2018-03-14 00:34:38 -0500

    working on dynamic memory

--- archi/interface.py
+++ archi/interface.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from archi.param import *
+import archi.param as param
 
 class interface(nn.Module):
     # an interface processor that takes all the interface output
@@ -12,23 +12,23 @@ class interface(nn.Module):
     def forward(self, interface_input):
         # TODO no initiation on assigned tensor here, see if it works
 
-        last_index=param_W*param_R
+        last_index=param.W*param.R
 
         # Read keys, each W dimensions, [W*R] in total
         # no processing needed
         # this is the address keys, not the contents
-        read_keys=interface_input[0:last_index].view(param_W,-1)
+        read_keys=interface_input[0:last_index].view(param.W,-1)
 
         # Read strengths, [R]
         # 1 to infinity
         # slightly different equation from the paper, should be okay
-        read_strengths=interface_input[last_index:last_index+param_R]
-        last_index=last_index+param_R
+        read_strengths=interface_input[last_index:last_index+param.R]
+        last_index=last_index+param.R
         read_strengths=1-nn.LogSigmoid(read_strengths)
 
         # Write key, [W]
-        write_key=interface_input[last_index:last_index+param_W]
-        last_index=last_index+param_W
+        write_key=interface_input[last_index:last_index+param.W]
+        last_index=last_index+param.W
 
         # write strength beta, [1]
         write_strength=interface_input[last_index:last_index+1]
@@ -36,17 +36,17 @@ class interface(nn.Module):
         write_strength=1-nn.LogSigmoid(write_strength)
 
         # erase strength, [W]
-        erase_vector=interface_input[last_index:last_index+param_W]
-        last_index=last_index+param_W
+        erase_vector=interface_input[last_index:last_index+param.W]
+        last_index=last_index+param.W
         erase_vector=nn.Sigmoid(erase_vector)
 
         # write vector, [W]
-        write_vector=interface[last_index:last_index+param_W]
-        last_index=last_index+param_W
+        write_vector=interface[last_index:last_index+param.W]
+        last_index=last_index+param.W
 
         # R free gates? [R] TODO what is this?
-        free_gates=interface[last_index:last_index+param_R]
-        last_index=last_index+param_R
+        free_gates=interface[last_index:last_index+param.R]
+        last_index=last_index+param.R
         free_gates=nn.Sigmoid(free_gates)
 
         # allocation gate [1]
@@ -60,11 +60,23 @@ class interface(nn.Module):
         write_gate=nn.Sigmoid(write_gate)
 
         # read modes [R]
-        read_modes=interface[last_index:last_index+param_R]
+        read_modes=interface[last_index:last_index+param.R]
         read_modes=nn.Softmax(read_modes)
 
-        # total dimension: param_W*param_R+3*param_W+5*param_R+3
-        # TODO I count a param_W feweer than it's supposed to have.
+        # total dimension: param.W*param.R+3*param.W+5*param.R+3
+        # TODO I count a param.W feweer than it's supposed to have.
         return read_keys, read_strengths, write_key, write_strength, \
                erase_vector, write_vector, free_gates, allocation_gate, \
                write_gate, read_modes
+
+
+# Maybe not, this means all the values will be stored in objects. No.
+# class Detupler(interface):
+#
+#     def __init__(self,interface):
+#         self.read_keys, self.read_strengths, self.write_key, self.write_strength, \
+#         self.erase_vector, self.write_vector, self.free_gates, self.allocation_gate, \
+#         self.write_gate, self.read_modes = interface
+#
+#
+
--- archi/memory.py
+++ archi/memory.py
@@ -1,21 +1,51 @@
 # this is the memory module
-# wow the dynamic memory thing is HARD.
+# wow the dynamic memory thing is hard.
+# cannot read the paper anymore. let's just implement it. hopefully by the end
+# I know what I'm doing.
 
+import torch
 import torch.nn as nn
-from torch.nn.functional import cosine_similarity
+from torch.nn.functional import cosine_similarity, softmax
+import archi.param as param
 
 class Memory(nn.Module):
 
     def __init__(self):
-        pass
+        memory_usage_u_t=torch.Tensor(param.N).zero_()
 
-    def content_lookup(self,lookup_key, memory, key_strength, index):
+    def content_lookup(self, desired_content, memory, key_strength):
         '''
 
-        :param lookup_key: (W) \in R
-        :param memory: (param_N, param_W)
-        :param key_strength: (1) \in [1, \infty)
-        :param index: lookup on memory[i]
-        :return:
+        :param desired_content: \k, (W), R, lookup key
+        :param memory: M, (param.N, param.W)
+        :param key_strength: \beta, (1) [1, \infty)
+        :param index: i, lookup on memory[i]
+        :return: most weighted similar: C, (param.N, 1), (0,1)
         '''
+
+        # TODO make sure the dimensions are correct.
+        similarties= cosine_similarity(desired_content, memory)
+        weighted=similarties*key_strength
+        return softmax(weighted)
+
+
+    # the highest freed will be retained? What does it mean?
+    def memory_retention(self,free_gate, read_weighting):
+        '''
+
+        :param free_gate: f, (R), [0,1], from interface vector
+        :param read_weighting: TODO calculated later, w, (N, R), simplex bounded
+        :return: \phi, (N), simplex bounded
+        '''
+
+
+        # a free gate belongs to a read head.
+        # a single read head weighting is a (N) dimensional simplex bounded value
+
+        # (N, R) TODO make sure this is pointwise multiplication, not matmul
+        inside_bracket = 1 - read_weighting * free_gate
+        return torch.prod(inside_bracket, 1)
+
+
+
 
