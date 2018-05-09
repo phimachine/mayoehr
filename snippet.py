732b65f1bb00c34d2ebd12961c16e04da9e93452
Jason Hu <huyaojie168@me.com>
2018-05-09 15:00:27 -0500

    update

--- snippet.py
+++ snippet.py
@@ -1,25 +1,27 @@
-from archi.computer import Computer
-import torch
-from pathlib import Path
-import os
-from torch.optim import SGD
-from os.path import abspath
+from threading import Thread
+import time
 
-def save_model(net, optim, epoch):
-    state_dict = net.state_dict()
-    for key in state_dict.keys():
-        state_dict[key] = state_dict[key].cpu()
-    task_dir = os.path.dirname(abspath(__file__))
-    print(task_dir)
-    pickle_file=Path("saves/DNC_"+str(epoch)+".pkl")
+class cachedYielder():
+    # this function runs a value generator when it's initiated, then everytime it's called, it returns the previous value and replenishes a new one
 
-    torch.save({
-        'epoch': 10,
-        'state_dict': state_dict,
-        'optimizer': optim},
-        pickle_file)
+    def __init__(self):
+        self.cache=None
+        self.count=0
+        self.gen()
 
-computer=Computer()
-optim=SGD(computer.parameters(),lr=0.1)
+    def gen(self):
+        # This is the slow generator
+        time.sleep(1)
+        self.cache=self.count
+        self.count+=1
+        print('generated and replaced')
 
-save_model(computer,optim,10)
+    def get_val(self):
+        Thread(target=self.gen).start()
+        return self.cache
+
+hello=cachedYielder()
+while(True):
+    needed=hello.get_val()
+    print("needed value accessed")
+    time.sleep(1)
