a33b3609d1b962168f528d505eeba016f66df99f
Jason Hu <huyaojie168@me.com>
2018-07-09 15:12:53 -0500

    Fixed services dictionary infinite loop caused bo NA.

--- death/post/inputgen_planA.py
+++ death/post/inputgen_planA.py
@@ -1,7 +1,8 @@
 from death.post.qdata import Dfs
 from torch.utils.data import Dataset, DataLoader
+import numpy as np
 
-# TODO coerce that there is no empty inputs, we can only assume that all deaths are recorded
+# we can only assume that all deaths are recorded
 
 # https://github.com/utkuozbulak/pytorch-custom-dataset-examples
 
@@ -11,6 +12,7 @@ class InputGen(Dataset):
     '''
     def __init__(self):
         self.dfs=Dfs()
+        self.rep_id=self.dfs.demo["rep_person_id"]
 
     def __getitem__(self, index):
         '''
@@ -23,16 +25,22 @@ class InputGen(Dataset):
         We do sliding window augmentation probably in the training stage.
         Simply because we cannot find the index for a window without loading the series.
 
+        input will be variable length, with a unit of a month
+
         :param index:
-        :return:
+        :return: (time, longest)
         '''
 
-        pass
+        id=self.rep_id[index]
+        # find death targets
+        
 
-    def
 
     def __len__(self):
         '''
         Length of the demographics dataset
         :return:
         '''
+
+if __name__=="__main__":
+    ig=InputGen()
