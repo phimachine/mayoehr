893815c1646e5b9b6c65dc7db083f84a3a3ae63d
Jason Hu <huyaojie168@me.com>
2018-07-17 16:52:36 -0500

    The problem is that controller produces two outputs that are wired into two different modules. When autograd happens, one module goes first and evaluates all the way to controller inputs, and the graph gets released. Then the second output evaluates, but the graph is no longer there. The solution is to combine all the modules into one, so that no graph gets released.

--- death/DNC/retaingraph.py
+++ death/DNC/retaingraph.py
@@ -42,49 +42,159 @@ from torch.autograd import Variable
 #     optim.step()
 #     hello.new_sequence_reset()
 
-class wrapper(nn.Module):
-    def __init__(self):
-        super(wrapper, self).__init__()
-        self.memory = Memory()
-        self.controller = MyController()
-        self.interface = Interface()
-        self.last_read_vector = Variable(torch.Tensor(param.bs, param.W, param.R).zero_().cuda())
-        # those work
 
+class memwrapper(nn.Module):
+    def __init__(self):
+        super(memwrapper, self).__init__()
+        self.inter=Interface()
+        self.mem=Memory()
+        self.ctrl=MyController()
         self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
 
+    def forward(self, input):
+        intout=self.inter(input)
+        read_vec=self.mem(*intout)
 
-    def forward(self,input):
-        # fake a time-series. (bs, ts, ...)
-        # only applies to the off-the-shelf LSTM
-        # input_x_t=input_x_t.unsqueeze(1)
-        input_x_t = torch.cat((input, self.last_read_vector.view(param.bs, -1)), dim=1)
+        output = torch.matmul(read_vec.view(param.bs, param.W * param.R),self.W_r)
 
-        output, interface = self.controller(input_x_t)
-        interface_output_tuple = self.interface(interface)
-        self.last_read_vector = self.memory(*interface_output_tuple)
-        # output = output + torch.matmul(self.last_read_vector.view(param.bs, param.W * param.R),self.W_r)
-
-        return output, self.last_read_vector
+        return output
 
     def new_sequence_reset(self):
-        # I have not found a reference to this function, but I think it's reasonable
-        # to reset the values that depends on a particular sequence.
-        self.controller.new_sequence_reset()
-        self.memory.new_sequence_reset()
-        self.last_read_vector = Variable(torch.Tensor(param.bs, param.W, param.R).zero_().cuda())
+        self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
+        self.mem.new_sequence_reset()
+
+# hello=memwrapper()
+# optim=torch.optim.Adam(hello.parameters())
+#
+# for i in range(100):
+#     x=Variable(torch.Tensor(1,param.E_t).uniform_(-1,1)).cuda()
+#     ctrx = Variable(torch.Tensor(1, param.x + param.R * param.W).uniform_(-1, 1)).cuda()
+#     output=hello(x)
+#     loss=output.sum()
+#     loss.backward()
+#     optim.step()
+#     hello.new_sequence_reset()
+#     print("success")
+
+class isadditiontheproblem(nn.Module):
+    def __init__(self):
+        super(isadditiontheproblem, self).__init__()
+        self.inter=Interface()
+        self.mem=Memory()
+        self.ctrl=MyController()
         self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
 
+    def forward(self, input,ctrinput):
+        intout=self.inter(input)
+        read_vec=self.mem(*intout)
 
-hello=wrapper()
+        aaa=self.ctrl(ctrinput)
+        bbb=torch.matmul(read_vec.view(param.bs, param.W * param.R),self.W_r)
+        output = aaa[0]+bbb
+        return output
+
+    def new_sequence_reset(self):
+        self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
+        self.mem.new_sequence_reset()
+        self.ctrl.new_sequence_reset()
+
+hello=isadditiontheproblem()
 optim=torch.optim.Adam(hello.parameters())
-for i in range(20):
-    x=Variable(torch.Tensor(1, param.x).uniform_(-1,1)).cuda()
-    output, lrv=hello(x)
-    loss=output.sum()+lrv.sum()
+#
+# for i in range(100):
+#     x=Variable(torch.Tensor(1,param.E_t).uniform_(-1,1)).cuda()
+#     ctrx = Variable(torch.Tensor(1, param.x + param.R * param.W).uniform_(-1, 1)).cuda()
+#     output=hello(x,ctrx)
+#     loss=output.sum()
+#     loss.backward()
+#     optim.step()
+#     hello.new_sequence_reset()
+#     print("what the heck")
+
+
+
+class issharinginputtheproblem(nn.Module):
+    def __init__(self):
+        super(issharinginputtheproblem, self).__init__()
+        self.inter=Interface()
+        self.mem=Memory()
+        self.ctrl=MyController()
+        self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
+
+    def forward(self, ctrinput):
+        output,intout=self.ctrl(ctrinput)
+        intout=self.inter(intout)
+
+        read_vec=self.mem(*intout)
+
+        bbb=torch.matmul(read_vec.view(param.bs, param.W * param.R),self.W_r)
+        output = output+bbb
+        return output
+
+    def new_sequence_reset(self):
+        self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
+        self.mem.new_sequence_reset()
+        self.ctrl.new_sequence_reset()
+
+hello=issharinginputtheproblem()
+optim=torch.optim.Adam(hello.parameters())
+
+for i in range(100):
+    x=Variable(torch.Tensor(1,param.E_t).uniform_(-1,1)).cuda()
+    ctrx = Variable(torch.Tensor(1, param.x + param.R * param.W).uniform_(-1, 1)).cuda()
+    output=hello(ctrx)
+    loss=output.sum()
     loss.backward()
     optim.step()
     hello.new_sequence_reset()
+    print("what the heck")
+
+#
+#
+# class wrapper(nn.Module):
+#     def __init__(self):
+#         super(wrapper, self).__init__()
+#         self.memory = Memory()
+#         self.controller = MyController()
+#         self.interface = Interface()
+#         self.last_read_vector = Variable(torch.Tensor(param.bs, param.W, param.R).zero_().cuda())
+#         # those work
+#
+#         self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
+#
+#
+#     def forward(self,input):
+#         # fake a time-series. (bs, ts, ...)
+#         # only applies to the off-the-shelf LSTM
+#         # input_x_t=input_x_t.unsqueeze(1)
+#         input_x_t = torch.cat((input, self.last_read_vector.view(param.bs, -1)), dim=1)
+#
+#         output, interface = self.controller(input_x_t)
+#         interface_output_tuple = self.interface(interface)
+#         self.last_read_vector = self.memory(*interface_output_tuple)
+#         ######### TODO THIS LINE IS THE PROBLEM ################
+#         # output = output + torch.matmul(self.last_read_vector.view(param.bs, param.W * param.R),self.W_r)
+#
+#         return output, self.last_read_vector
+#
+#     def new_sequence_reset(self):
+#         # I have not found a reference to this function, but I think it's reasonable
+#         # to reset the values that depends on a particular sequence.
+#         self.controller.new_sequence_reset()
+#         self.memory.new_sequence_reset()
+#         self.last_read_vector = Variable(torch.Tensor(param.bs, param.W, param.R).zero_().cuda())
+#         self.W_r = Parameter(torch.Tensor(param.W * param.R, param.v_t).zero_().cuda())
+#
+#
+# hello=wrapper()
+# optim=torch.optim.Adam(hello.parameters())
+# for i in range(20):
+#     x=Variable(torch.Tensor(1, param.x).uniform_(-1,1)).cuda()
+#     output, lrv=hello(x)
+#     loss=output.sum()+lrv.sum()
+#     loss.backward()
+#     optim.step()
+#     hello.new_sequence_reset()
 
 
 
