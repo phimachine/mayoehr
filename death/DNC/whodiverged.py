caa910d0582b2c2c0ba5aec932d6ce947c25f258
Jason Hu <huyaojie168@me.com>
2018-07-25 15:10:25 -0500

    DNC is a failure. Starting Tacotron.

--- death/DNC/whodiverged.py
+++ death/DNC/whodiverged.py
@@ -33,126 +33,15 @@ def save_model(net, optim, epoch, iteration):
     torch.save((net,  optim, epoch, iteration), pickle_file)
     print('model saved at', pickle_file)
 
-def save_model_old(net, optim, epoch, iteration):
-    print("saving model")
-    epoch = int(epoch)
-    state_dict = net.state_dict()
-    for key in state_dict.keys():
-        state_dict[key] = state_dict[key].cpu()
-    task_dir = os.path.dirname(abspath(__file__))
-    pickle_file = Path(task_dir).joinpath("saves/DNCfull_" + str(epoch) + "_" + str(iteration) + ".pkl")
-    fhand = pickle_file.open('wb')
-    try:
-        pickle.dump((state_dict,optim, epoch, iteration),fhand)
-        print('model saved at', pickle_file)
-    except:
-        fhand.close()
-        os.remove(pickle_file)
-
 def load_model(computer, optim, starting_epoch, starting_iteration):
     task_dir = os.path.dirname(abspath(__file__))
-    save_dir = Path(task_dir) / "saves"
-    highestepoch = 0
-    highestiter = 0
-    for child in save_dir.iterdir():
-        epoch = str(child).split("_")[3]
-        iteration = str(child).split("_")[4].split('.')[0]
-        iteration = int(iteration)
-        epoch = int(epoch)
-        # some files are open but not written to yet.
-        if child.stat().st_size > 20480:
-            if epoch > highestepoch or (iteration > highestiter and epoch==highestepoch):
-                highestepoch = epoch
-                highestiter = iteration
-    if highestepoch == 0 and highestiter == 0:
-        print("nothing to load")
-        return computer, optim, starting_epoch, starting_iteration
-    pickle_file = Path(task_dir).joinpath("saves/DNCfull_" + str(highestepoch) + "_" + str(highestiter) + ".pkl")
+    pickle_file = Path(task_dir).joinpath("DNCcrash.pkl")
     print("loading model at", pickle_file)
     pickle_file = pickle_file.open('rb')
     computer, optim, epoch, iteration = torch.load(pickle_file)
-    print('Loaded model at epoch ', highestepoch, 'iteartion', iteration)
-    #
-    # for child in save_dir.iterdir():
-    #     epoch = str(child).split("_")[3].split('.')[0]
-    #     iteration = str(child).split("_")[4].split('.')[0]
-    #     if int(epoch) != highestepoch and int(iteration) != highestiter:
-    #         # TODO might have a bug here.
-    #         os.remove(child)
-    # print('Removed incomplete save file and all else.')
-
-    return computer, optim, highestepoch, highestiter
-
-
-def load_model_old(computer):
-    task_dir = os.path.dirname(abspath(__file__))
-    save_dir = Path(task_dir) / "saves"
-    highestepoch = -1
-    highestiter = -1
-    for child in save_dir.iterdir():
-        epoch = str(child).split("_")[3]
-        iteration = str(child).split("_")[4].split('.')[0]
-        iteration=int(iteration)
-        epoch = int(epoch)
-        # some files are open but not written to yet.
-        if epoch > highestepoch and iteration>highestiter and child.stat().st_size > 204800:
-            highestepoch = epoch
-            highestiter=iteration
-    if highestepoch == -1 and highestepoch==-1:
-        return computer, None, -1, -1
-    pickle_file = Path(task_dir).joinpath("saves/DNCfull_" + str(highestepoch)+"_"+str(iteration) + ".pkl")
-    print("loading model at ", pickle_file)
-    pickle_file = pickle_file.open('rb')
-    modelsd, optim, epoch, iteration = torch.load(pickle_file)
-    computer.load_state_dict(modelsd)
-    print('Loaded model at epoch ', highestepoch, 'iteartion', iteration)
-
-    for child in save_dir.iterdir():
-        epoch = str(child).split("_")[3].split('.')[0]
-        iteration = str(child).split("_")[4].split('.')[0]
-        if int(epoch) != highestepoch and int(iteration) != highestiter:
-            os.remove(child)
-    print('Removed incomplete save file and all else.')
 
     return computer, optim, epoch, iteration
 
-
-def salvage():
-    # this function will pick up the last two highest epoch training and save them somewhere else,
-    # this is to prevent unexpected data loss.
-    # We are working in a /tmp folder, and we write around 1Gb per minute.
-    # The loss of data is likely.
-
-    task_dir = os.path.dirname(abspath(__file__))
-    save_dir = Path(task_dir) / "saves"
-    highestepoch = -1
-    secondhighestiter = -1
-    highestiter = -1
-    for child in save_dir.iterdir():
-        epoch = str(child).split("_")[3]
-        iteration = str(child).split("_")[4].split('.')[0]
-        iteration = int(iteration)
-        epoch = int(epoch)
-        # some files are open but not written to yet.
-        if epoch > highestepoch and iteration > highestiter and child.stat().st_size > 20480:
-            highestepoch = epoch
-            highestiter = iteration
-    if highestepoch == -1 and highestiter == -1:
-        print("no file to salvage")
-        return
-    if secondhighestiter != -1:
-        pickle_file2 = Path(task_dir).joinpath("saves/DNCfull_" + str(highestepoch) + "_" + str(secondhighestiter) + ".pkl")
-        copy(pickle_file2, "/infodev1/rep/projects/jason/pickle/salvage2.pkl")
-
-    pickle_file1 = Path(task_dir).joinpath("saves/DNCfull_" + str(highestepoch) + "_" + str(highestiter) + ".pkl")
-    copy(pickle_file1, "/infodev1/rep/projects/jason/pickle/salvage1.pkl")
-
-    print('salvaged, we can start again with /infodev1/rep/projects/jason/pickle/salvage1.pkl')
-
-def run_one_patient_one_step():
-    # this is so python does garbage collection automatically.
-    # we are debugging the
-    pass
 
 global_exception_counter=0
 def run_one_patient(computer, input, target, target_dim, optimizer, loss_type, real_criterion,
@@ -290,7 +179,7 @@ def forevermain():
             pass
 
 
-def main(load=True):
+def probe():
     total_epochs = 10
     iter_per_epoch = 100000
     lr = 1e-3
@@ -310,9 +199,8 @@ def main(load=True):
     computer=DNC()
 
     # load model:
-    if load:
-        print("loading model")
-        computer, optim, starting_epoch, starting_iteration = load_model(computer, optim, starting_epoch, starting_iteration)
+    print("loading model")
+    computer, optim, starting_epoch, starting_iteration = load_model(computer, optim, starting_epoch, starting_iteration)
 
     computer = computer.cuda()
     if optim is None:
@@ -332,4 +220,5 @@ def main(load=True):
 
 
 if __name__ == "__main__":
-    main()
+    with torch.cuda.device(1):
+        probe()
