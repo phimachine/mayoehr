02e71f81b9ebac4c3bf2237e97155876ab049057
fuchai <huyaojie168@me.com>
2018-09-11 22:17:08 -0500

    Augmenting code in R does not seem reasonable. Look at dia$dx_code. Bar separated codes are not easily processed with R. Must be done in Python.

--- death/post/post.R
+++ death/post/post.R
@@ -124,3 +124,29 @@ foreach(dfn=dfs) %dopar%{
     fwrite(df,paste('/infodev1/rep/projects/jason/multi',dfn,'.csv',sep=''))
     1
 }
+
+
+###################
+# it feed the code structure into the net.
+
+# this script augments the dataset codes by inserting more codes
+# by lowering the precision of the precise codes.
+
+source("loadall.R")
+require(stringr)
+
+# this is a function that spawns multiple columns with one column
+# depth refers to the maximal depth of a column code. E.g., A1B2C3 has depth 6.
+# When code is A1B, we pad the code to have depth 6 then start splitting.
+
+spawn <- function(dt, col, depth){
+    dt <- get(dt)
+    dt <- dt %>% mutate(!!col:=str_pad(dt[[col]],depth,side="right",pad="0"))
+    dt
+}
+sptb <- data.table(
+dt = character(),
+col = character(),
+depth = numeric()
+)
+rbindlist(list(sptb, list("death","code",5), list(
