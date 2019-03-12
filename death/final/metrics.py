Now DNC has a fixed prior parameter to be the training dataset p(y) and predicts likelihood instead. Fixed some bugs in confusion metrics log underflow and overflow. Added a statement in input generator that prevents adding the same code more than once if it appeared more than once.

death/final/metrics.py
@@ -56,7 +56,7 @@ class ConfusionMatrixStats():
         self.true_negative[:,self.idx]=true_negative
 
         batch_sensitivity=np.mean(true_positive/conditional_positive.clip(1e-8,None), axis=0)
-        batch_specificity=np.mean(true_negative/conditional_negative.clip(1e-8,None),axis=0)
+        batch_specificity=np.mean(true_negative/conditional_negative.clip(1e-8,None), axis=0)
         batch_ROC=batch_sensitivity+batch_specificity
 
         assert((batch_sensitivity>0).all())
@@ -74,20 +74,20 @@ class ConfusionMatrixStats():
 
     def running_stats(self):
         if self.all:
-            running_sensitivity=np.mean(np.sum(self.true_positive,axis=1)/np.sum(self.conditional_positives,axis=1), axis=0)
-            running_specificity=np.mean(np.sum(self.true_negative,axis=1)/np.sum(self.conditional_negatives,axis=1), axis=0)
+            running_sensitivity=np.mean(np.sum(self.true_positive,axis=1)/np.sum(self.conditional_positives,axis=1).clip(min=1e-8), axis=0)
+            running_specificity=np.mean(np.sum(self.true_negative,axis=1)/np.sum(self.conditional_negatives,axis=1).clip(min=1e-8), axis=0)
             running_ROC=running_sensitivity+running_specificity
         else:
             running_sensitivity=np.mean(np.sum(self.true_positive[:,:self.idx],axis=1)/
-                                        np.sum(self.conditional_positives[:,:self.idx],axis=1), axis=0)
+                                        np.sum(self.conditional_positives[:,:self.idx],axis=1).clip(min=1e-8), axis=0)
             running_specificity=np.mean(np.sum(self.true_negative[:,:self.idx],axis=1)/
-                                        np.sum(self.conditional_negatives[:,:self.idx],axis=1), axis=0)
+                                        np.sum(self.conditional_negatives[:,:self.idx],axis=1).clip(min=1e-8), axis=0)
             running_ROC=running_sensitivity+running_specificity
         assert((running_sensitivity>0).all())
         assert((running_sensitivity<1).all())
         assert((running_specificity>0).all())
         assert((running_specificity<1).all())
-        assert((running_ROC>1).all())
+        assert((running_ROC>0).all())
         assert((running_ROC<2).all())
 
         return running_sensitivity, running_specificity, running_ROC

death/post/inputgen_planJ.py
@@ -287,7 +287,7 @@ class InputGen(Dataset, DFManager):
             while word !="":
                 struct_code_list.append(dic[word]+codelist[0])
                 word=word[:-1]
-            indices[1]=struct_code_list
+            indices[1]=list(set(struct_code_list))
             # remove all structured codes before
             # this causes target to be bigger than 1 therefore BCE cannot be applied.
             np.multiply.at(array,indices,0)